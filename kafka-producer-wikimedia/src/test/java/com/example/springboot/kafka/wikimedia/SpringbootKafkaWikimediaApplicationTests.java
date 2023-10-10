dd5c64db3a09afcf056a50b3afdfa8cca4c209b2
Olha Lopushanska <55944118+OlgaLopushanska@users.noreply.github.com>
2023-10-10 17:15:57 +0300

    add app

---
+++ kafka-producer-wikimedia/src/test/java/com/example/springboot/kafka/wikimedia/SpringbootKafkaWikimediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.kafka.wikimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaWikimediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
